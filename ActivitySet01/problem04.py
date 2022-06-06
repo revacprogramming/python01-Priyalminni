d33e161ac88fc18eceb0a168937672ad880c6962
PRIYAL MINNI <93091977+Priyalminni@users.noreply.github.com>
2022-06-06 03:23:19 +0000

    executed

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,8 @@
-# Conditional Execution
+hrs = float(input("Enter Hours:"))
 
-hrs = input("Enter hours? ")
+xx =  float(input("Enter the Rate:"))
+
+if hrs <= 40:
+ 	print( h  * xx)
+elif h > 40:
+	print(40* xx + (hrs-40)*1.5*xx)
